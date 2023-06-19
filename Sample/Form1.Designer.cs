43308a8c4f400bb7adad4be217dde593b938caf9
mbaibatyr <murat_b@mail.ru>
2023-06-19 21:42:21 +0600

    --

---
+++ Sample/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgAgents = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgAgents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(405, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ExcelDataReader";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgAgents
+            // 
+            this.dgAgents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgAgents.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgAgents.Location = new System.Drawing.Point(0, 67);
+            this.dgAgents.Name = "dgAgents";
+            this.dgAgents.Size = new System.Drawing.Size(800, 451);
+            this.dgAgents.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(632, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "closedXML";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(725, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(37, 18);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Sales";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 518);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dgAgents);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgAgents)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgAgents;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Sample/Form1.cs
@@ -0,0 +1,187 @@
+﻿using ClosedXML.Excel;
+using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //using (var stream = File.Open(@"C:\temp\срок аренды.xlsx", FileMode.Open, FileAccess.Read))
+            using (var stream = File.Open(@"C:\temp\книга1.xlsx", FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var result = reader.AsDataSet();
+                    dgAgents.DataSource = result.Tables[0];
+                    DateTime dt;
+                    foreach (DataRow row in result.Tables[0].Rows)
+                    {
+                        if (DateTime.TryParse(row[4].ToString(), out dt))
+                            MessageBox.Show(dt.ToString());
+                        else
+                            MessageBox.Show(row[4].ToString());
+                    }
+                }
+            }
+        }
+
+        private static string GetConnectionString(string file)
+        {
+            Dictionary<string, string> props = new Dictionary<string, string>();
+
+            string extension = file.Split('.').Last();
+
+            if (extension == "xls")
+            {
+                //Excel 2003 and Older
+                props["Provider"] = "Microsoft.Jet.OLEDB.4.0";
+                props["Extended Properties"] = "Excel 8.0";
+            }
+            else if (extension == "xlsx")
+            {
+                //Excel 2007, 2010, 2012, 2013
+                props["Provider"] = "Microsoft.ACE.OLEDB.12.0;";
+                props["Extended Properties"] = "Excel 12.0 XML";
+            }
+            else
+                throw new Exception(string.Format("error file: {0}", file));
+
+            props["Data Source"] = file;
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (KeyValuePair<string, string> prop in props)
+            {
+                sb.Append(prop.Key);
+                sb.Append('=');
+                sb.Append(prop.Value);
+                sb.Append(';');
+            }
+
+            return sb.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fileName = @"C:\temp\срок аренды.xlsx";
+            using (var excelWorkbook = new XLWorkbook(fileName))
+            {
+                var nonEmptyDataRows = excelWorkbook.Worksheet(1).RowsUsed();
+
+                foreach (var dataRow in nonEmptyDataRows)
+                {
+                    //for row number check
+                    //if (dataRow.RowNumber() >= 3 && dataRow.RowNumber() <= 20)
+                    {
+                        dataRow.Cell(10).Style.NumberFormat.Format = "dd.MM.yyyy";
+                        DateTime dt;
+
+                        if (DateTime.TryParse(dataRow.Cell(10).Value.ToString(), out dt))
+                            Text = dt.ToString();
+
+
+                        string st = $"{dataRow.Cell(8).Value.ToString()} " +
+                            $"{dataRow.Cell(10).Value}  " +
+                            $"{dataRow.Cell(11).Value.ToString()}";
+                        Text = st;
+
+                        //Thread.Sleep(3000);
+
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            DataSet ds = new DataSet();
+
+            string connectionString = GetConnectionString(@"C:\temp\срок аренды.xlsx");
+
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                conn.Open();
+                OleDbCommand cmd = new OleDbCommand();
+                cmd.Connection = conn;
+
+                // Get all Sheets in Excel File
+                DataTable dtSheet = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                // Loop through all Sheets to get data
+                foreach (DataRow dr in dtSheet.Rows)
+                {
+                    string sheetName = dr["TABLE_NAME"].ToString();
+
+                    if (!sheetName.EndsWith("$"))
+                        continue;
+
+                    // Get all rows from the Sheet
+                    cmd.CommandText = "SELECT * FROM [" + sheetName + "]";
+
+                    DataTable dt = new DataTable();
+                    dt.TableName = sheetName;
+
+                    OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                    da.Fill(dt);
+
+                    ds.Tables.Add(dt);
+                }
+
+                cmd = null;
+                conn.Close();
+            }
+
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DataTable dtExcel = new DataTable();
+            DataTable dtDB = new DataTable();
+            using (var stream = File.Open(@"C:\temp\книга1.xlsx", FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var result = reader.AsDataSet();
+                    dtExcel = result.Tables[0];
+                    dtExcel.Rows[0].Delete();
+                }
+            }
+
+            using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\байбатыровм\Documents\testDB.mdf;Integrated Security=True;Connect Timeout=30"))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("select lastName, [month], factSales from sales", con))
+                {
+                    dtDB.Load(cmd.ExecuteReader());  
+                }
+                con.Close();
+            }
+
+            foreach (DataRow item in dtDB.Rows)
+            {
+                var dr = dtExcel.Select("f2 = '" + item[0].ToString() + "'");
+            }
+        }
+    }
+}
